376b4b284b89f19a0d827de6eaea8bb63caac498
Peter Balazs <Peter.Balazs@itron.com>
2021-05-21 00:23:30 +0200

    Read all the data from the flash

--- dataReader.py
+++ dataReader.py
@@ -10,7 +10,7 @@ def callback(sender, data):
     global globalData
     global globalDataLen
     
-    # Notification came from the right handle?
+    # Did notification come from the right handle?
     if sender == config.COM_CHAR_HANDLE:
         
         # Is this a response for memory read command?
@@ -18,7 +18,7 @@ def callback(sender, data):
         
             len = data[1]
             
-            # At the moment we assume the responds come in the correct order.
+            # At the moment we assume the responses come in the correct order.
             # This can be improved in the future.
             
             globalData += data[4:4+len]
@@ -30,6 +30,7 @@ async def readMemory(client, startAddr, length):
     global globalData
     global globalDataLen
     
+    globalData = bytearray()
     globalDataLen = 0
     await client.start_notify(config.COM_CHAR_HANDLE, callback)
     
@@ -41,6 +42,22 @@ async def readMemory(client, startAddr, length):
         await asyncio.sleep(0.3)
         
     return globalData
+    
+    
+async def readEntireFlash(client):
+
+    # Read first page
+    # For now let's assume the rest of the pages are empty..
+    header = await readMemory(client, 0, 3)
+    
+    if header[0] != 0x54:
+        raise Exception("wrong header")
+        
+    # If there is measurement data, read it
+    if header[2] > 0:
+        body = await readMemory(client, 8, header[2])
+    
+    return body
     
 
 async def run(address):
@@ -54,11 +71,9 @@ async def run(address):
         
         # Move buffered data to flash (to prepare it for reading)
         await client.write_gatt_char(config.COM_CHAR_HANDLE, bytearray(b'\x30'))
-        
-        #Read memory and interpret it
-        
-        # Read 30 bytes for test
-        res = await readMemory(client, 0, 30)
+                
+        # Read all of the data in the flash
+        res = await readEntireFlash(client)
         
         print(res)
 
